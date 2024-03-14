ba98bb88cf6abfb71d3c1e34674fa234069e8b55
moodles <rubbersoul11@163.com>
2024-03-14 19:43:22 +0800

    add function: auto run

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const generateBtn = document.querySelector("#generate-btn");
 const resetBtn = document.querySelector("#reset-btn");
+const autoBtn = document.querySelector("#auto-btn");
 const colorSet = [
   "red",
   "orange",
@@ -10,6 +11,7 @@ const colorSet = [
   "blue",
   "purple",
 ];
+let gridSize = 16;
 
 function appendDivToGrid(gridSize) {
   const divSize = 512 / gridSize;
@@ -28,7 +30,6 @@ function initGrid() {
 
 function generateGrid() {
   gridContainer.innerHTML = "";
-  let gridSize;
 
   do {
     gridSize = parseInt(prompt("Please enter the grid size (max 100)"));
@@ -41,10 +42,25 @@ function addHoverEffect() {
   this.style.backgroundColor = color;
 }
 
+function autoRun() {
+  gridContainer.innerHTML = "";
+  const divSize = 512 / gridSize;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement("div");
+    const color = colorSet[Math.floor(Math.random() * colorSet.length)];
+    div.setAttribute(
+      "style",
+      `width: ${divSize}px; height: ${divSize}px; background-color: ${color};`
+    );
+    gridContainer.appendChild(div);
+  }
+}
+
 function main() {
   initGrid();
   generateBtn.addEventListener("click", generateGrid);
   resetBtn.addEventListener("click", initGrid);
+  autoBtn.addEventListener("click", autoRun);
 }
 
 main();
